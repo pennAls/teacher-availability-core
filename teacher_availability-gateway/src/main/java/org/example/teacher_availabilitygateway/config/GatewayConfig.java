bc844bafe6684079c171800c22b22acbea5da4d0
pennA <143518683+pennAls@users.noreply.github.com>
2026-06-02 17:58:49 -0300

    feat: adiciona mapeamento de todas as rotas no gateway

--- teacher_availability-gateway/src/main/java/org/example/teacher_availabilitygateway/config/GatewayConfig.java
+++ teacher_availability-gateway/src/main/java/org/example/teacher_availabilitygateway/config/GatewayConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
@@ -17,6 +18,30 @@ public class GatewayConfig {
         return route("ms-security")
                 .route(path("/auth/**", "/users/**"), http())
                 .filter(lb("MS-SECURITY"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> academicRoute() {
+        return route("ms-academic")
+                .route(path("/ies/**", "/schools/**", "/disciplines/**"), http())
+                .filter(lb("MS-ACADEMIC"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> teacherRoute() {
+        return route("ms-teacher")
+                .route(path("/teachers/**", "/degrees/**"), http())
+                .filter(lb("MS-TEACHER"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> planningRoute() {
+        return route("ms-planning")
+                .route(path("/availabilities/**", "/interests/**"), http())
+                .filter(lb("MS-PLANNING"))
                 .build();
     }
 }
